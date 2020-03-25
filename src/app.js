cda6892867c1287fd628e2a85857adcd89a5ea89
izaacAbrams <izaac.abrams25@gmail.com>
2020-03-24 22:12:27 -0500

    preparing for production

--- src/app.js
+++ src/app.js
@@ -20,10 +20,10 @@ app.use("/api/notes", notesRouter);
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
+  console.log(error);
   if (NODE_ENV === "production") {
     response = { error: { message: "server error" } };
   } else {
-    console.log(error);
     response = { message: error.message, error };
   }
   res.status(500).json(response);
